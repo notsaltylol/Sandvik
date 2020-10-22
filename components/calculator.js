0a3bc5684e1f6bae21b7f7462d9ccd5959d202d5
Daniel Bertak <danber8@ufl.edu>
2020-10-22 09:35:52 -0400

    Added Scrolling and button to calculator

--- components/calculator.js
+++ components/calculator.js
@@ -1,24 +1,35 @@
 import React, { useState } from 'react'
-import {StyleSheet, TextInput, Button, View, Alert, Text} from 'react-native'
+import {StyleSheet, TextInput, Button, View, Alert, Text, ScrollView} from 'react-native'
 import styles from '../styles.js'
 
 const Calculator = () => {
     const [holeDiameter, setHoleDiameter] = useState(null);
-    const [holeDepth, setHoleDepth] = useState(null)
+    const [holeDepth, setHoleDepth] = useState(null);
+
+    const pressHandler = () =>{
+        Alert.alert('Attempting to calculate something')
+      }
     return(
         <View styles={styles.container}>
-          <Text> Enter hole diameter</Text>
-          <TextInput
-            style={styles.input}
-            onChangeText={text => setHoleDiameter(text)}
-          />
+            <ScrollView>
+                <Text> Enter hole diameter</Text>
+                <TextInput
+                    keyboardType='numeric'
+                    style={styles.input}
+                    onChangeText={text => setHoleDiameter(text)}
+                />
 
-          <Text> Enter hole depth</Text>
-            <TextInput
-            style={styles.input}
-            onChangeText={text => setHoleDepth(text)}
-            
-            />
+                <Text> Enter hole depth</Text>
+                    <TextInput
+                    keyboardType='numeric'
+                    style={styles.input}
+                    onChangeText={text => setHoleDepth(text)}
+                    
+                    />
+                    <View style={styles.buttonContainer}>
+                        <Button title='Calculate' onPress={pressHandler}/>
+                    </View>
+            </ScrollView>
 
         
         
