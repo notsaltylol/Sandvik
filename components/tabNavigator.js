497048528072d213a11ad5d947f3228c7379ca8d
Daniel Bertak <danber8@ufl.edu>
2020-10-27 20:39:34 -0400

    Slight change to tab navigator

--- components/tabNavigator.js
+++ components/tabNavigator.js
@@ -14,9 +14,7 @@ const tabNavigator = ({setIsSignedIn}) => {
                 <Tab.Screen name="Calculator" component={Calculator} />
                 <Tab.Screen name="Upload CSV" component={uploadCSV} />
                 {/* <Tab.Screen name="View Results" component={ViewResults} /> */}
-                <Tab.Screen name="LogOut"  >
-                {()=><LogOut setIsSignedIn={setIsSignedIn}/>}
-                </Tab.Screen>
+                <Tab.Screen name="LogOut" children={()=><LogOut setIsSignedIn={setIsSignedIn}/>} />
             </Tab.Navigator>
         </NavigationContainer>
     )
