580578fe7f8d4fd5ced2e259c6589f42e5d82401
Robin Fintz <robinfintz@ufl.edu>
2020-10-31 15:30:57 -0400

    Added more inputs for generic form

--- components/calculator.js
+++ components/calculator.js
@@ -8,6 +8,10 @@ import {ftToMeters, metersToFeet} from '../mathFunctions.js'
 const Calculator = () => {
     const [elevationMeters, setElevationMeters] = useState(() => {return '0'});
     const [elevationFt, setElevationFt] = useState(() => {return '0'});
+    const [benchHeightMeters, setBenchHeightMeters] = useState(() => {return '0'});
+    const [benchHeightFt, setBenchHeightFt] = useState(() => {return '0'});
+    const [subDrillingMeters, setSubDrillingMeters] = useState(() => {return '0'});
+    const [subDrillingFt, setSubDrillingFt] = useState(() => {return '0'});
     const [holeDepth, setHoleDepth] = useState(null);
     const [customerName, setCustomerName] = useState('');
 
@@ -31,6 +35,10 @@ const Calculator = () => {
                 </View>
 
                 <GenericTwoInput title={'Elevation'} val1={elevationMeters} val2={elevationFt} setFunction1={setElevationFt} setFunction2={setElevationMeters} func1={metersToFeet} func2={ftToMeters} unit1={'m'} unit2={'ft'}></GenericTwoInput>
+                <GenericTwoInput title={'Bit Size'} val1={elevationMeters} val2={elevationFt} setFunction1={setElevationFt} setFunction2={setElevationMeters} func1={metersToFeet} func2={ftToMeters} unit1={'mm'} unit2={'in'}></GenericTwoInput>
+                <GenericTwoInput title={'Drill Pipe OD'} val1={elevationMeters} val2={elevationFt} setFunction1={setElevationFt} setFunction2={setElevationMeters} func1={metersToFeet} func2={ftToMeters} unit1={'mm'} unit2={'in'}></GenericTwoInput>
+                <GenericTwoInput title={'Bench Height'} val1={benchHeightMeters} val2={benchHeightFt} setFunction1={setBenchHeightFt} setFunction2={setBenchHeightMeters} func1={metersToFeet} func2={ftToMeters} unit1={'m'} unit2={'ft'}></GenericTwoInput>
+                <GenericTwoInput title={'Sub-Drilling'} val1={subDrillingMeters} val2={subDrillingFt} setFunction1={setSubDrillingFt} setFunction2={setSubDrillingMeters} func1={metersToFeet} func2={ftToMeters} unit1={'m'} unit2={'ft'}></GenericTwoInput>
 
                 <Text> Enter hole depth</Text>
                     <TextInput
