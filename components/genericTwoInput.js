f1d43b3c27117526c74d43bfad9d3d2456a940e6
Daniel Bertak <danber8@ufl.edu>
2020-11-08 17:30:42 -0500

    Fixed generic Two input

--- components/genericTwoInput.js
+++ components/genericTwoInput.js
@@ -6,51 +6,44 @@ import styles from '../styles.js'
 
 const GenericTwoInput = ({ title, val, setFunction, unit }) => {
     
-    /* const onTypeBox1 = (unit) => {
-        setFunction2(unit.toString())
-        setFunction1(func1(unit).toString())
+    const onTypeBox1 = (unit) => {
+        setFunction(unit.toString())
     }
-    
-    
-    const onTypeBox2 = (unit) => {
-        setFunction1(unit.toString())
-        setFunction2(func2(unit).toString())
-    } */
 
     const [positionX,setPositionX]=useState(150)
     const [positionY,setPositionY]=useState(200)
 
     return(
         <View>
-                    <View style={styles.rowStyle}>
-                        <Text style={styles.units}>{unit}</Text>
-                        <View>
-                            <View style={styles.rowStyle }>
-                                <TextInput
-                                    value={val}
-                                    keyboardType='numeric'
-                                    style={styles.calcInput}
-                                    onChangeText={(text) => onTypeBox1(text)}
-                                    placeholder = {unit}
-                                />
-                                <Text style={styles.inputTitle}>{title}</Text>
-                            </View>
-
-                            {/* <View style={styles.rowStyle}>
-                                <TextInput
-                                    value={val2}
-                                    keyboardType='numeric'
-                                    style={styles.input}
-                                    onChangeText={text => onTypeBox2(text)}
-                                />
-                                <Text>{unit2}</Text>
-                            </View> */}
-
-
-                        </View>
+            <View style={styles.rowStyle}>
+                <Text style={styles.units}>{unit}</Text>
+                <View>
+                    <View style={styles.rowStyle }>
+                        <TextInput
+                            value={val}
+                            keyboardType='numeric'
+                            style={styles.calcInput}
+                            onChangeText={(text) => onTypeBox1(text)}
+                            placeholder = {unit}
+                        />
+                        <Text style={styles.inputTitle}>{title}</Text>
                     </View>
-                    
+
+                    {/* <View style={styles.rowStyle}>
+                        <TextInput
+                            value={val2}
+                            keyboardType='numeric'
+                            style={styles.input}
+                            onChangeText={text => onTypeBox2(text)}
+                        />
+                        <Text>{unit2}</Text>
+                    </View> */}
+
+
                 </View>
+            </View>
+            
+        </View>
     )
 }
 
