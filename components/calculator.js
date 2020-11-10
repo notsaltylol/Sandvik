4e9541e74fdb730dab0aa66c96c4338d5e1d5d4e
Daniel Bertak <danber8@ufl.edu>
2020-11-09 19:42:45 -0500

    refactored calculator

--- components/calculator.js
+++ components/calculator.js
@@ -17,54 +17,54 @@ const windowWidth = Dimensions.get('window').width;
 
 
 const Calculator = () => {
-    const [bit, setBit] = useState(() => {return '7 7/8'});
-    const [burden, setBurden] = useState(() => {return 5.5})
-    const [spacing, setSpacing] = useState(() => {return 6.1})
-    const [subDrilling, setSubDrilling] = useState(() => {return '1.2'})
-    const [bench, setBench] = useState(() => {return 12})
-    const [rockDensityTon, setRockDensityTon] = useState(() => {return '2.75'})
-    const [rockDensityUCS, setRockDensityUCS] = useState(() => {return '157'})
+    const [D3, setD3] = useState(() => {return '7 7/8'});
+    const [D4, setD4] = useState(() => {return 5.5})
+    const [D5, setD5] = useState(() => {return 6.1})
+    const [D6, setD6] = useState(() => {return '1.2'})
+    const [D7, setD7] = useState(() => {return 12})
+    const [D8, setD8] = useState(() => {return '2.75'})
+    const [D9, setD9] = useState(() => {return '157'})
 
-    const [tonHole, setTonHole] = useState(tonHoleCalculation(bench, burden, spacing, rockDensityTon));
-    useEffect(()=> {
-        setTonHole(tonHoleCalculation(bench, burden, spacing, rockDensityTon));
-    }, [bench, burden, spacing, rockDensityTon])
-
-    const [drillingIndex, setDrillingIndex] = useState(drillingIndexCalculation(burden, spacing, rockDensityTon))
+    const [D10, setD10] = useState(drillingIndexCalculation(D4, D5, D8))
     useEffect(() => {
-        setDrillingIndex(drillingIndexCalculation(burden, spacing, rockDensityTon))
-    }, [burden, spacing, rockDensityTon])
+        setD10(drillingIndexCalculation(D4, D5, D8))
+    }, [D4, D5, D8])
+
+    const [D11, setD11] = useState(tonHoleCalculation(D7, D4, D5, D8));
+    useEffect(()=> {
+        setD11(tonHoleCalculation(D7, D4, D5, D8));
+    }, [D7, D4, D5, D8])
     
-    const [targetProduction, setTargetProduction] = useState('872321')
-    const [numHoles, setNumHoles] = useState('685')
-    const [mMonth, setMMonth] = useState('85093')
-    const [utilizedHours, setUtilizedHours] = useState('511')
-    const [penRate, setPenRate] = useState('21.5')
+    const [D12, setD12] = useState('872321')
+    const [D13, setD13] = useState('685')
+    const [D14, setD14] = useState('85093')
+    const [D15, setD15] = useState('511')
+    const [D16, setD16] = useState('21.5')
 
     //Holes	Metres	Hours	m/hr	Total_Ton	% of Target
-    var holesA = '788';
-    var holesB = '832';
-    var holesC = '286';
-    var holesD = '286';
-    var holesE = '300';
+    const [H10, setH10] = useState(788);
+    const [H11, setH11] = useState(832);
+    const [H12, setH12] = useState(286);
+    const [H13, setH13] = useState(286);
+    const [H14, setH14] = useState(300);
 
-    var metresA = '11825';
-    var metresB = '10897';
-    var metresC = '3771';
-    var metresD = '3771';
-    var metresE = '3954';
+    const [I10, setI10] = useState(11825);
+    const [I11, setI11] = useState(10897);
+    const [I12, setI12] = useState(3771);
+    const [I13, setI13] = useState(3771);
+    const [I14, setI14] = useState(3954);
 
     const [hours, setHours] = useState(() => {return 'Hours'})
-    var hoursB = utilizedHours;
-    var hoursC = '175';
-    var hoursD = '175';
-    var hoursE = '175';
+    var J11 = D15;
+    var J12 = 175;
+    var J13 = 175;
+    var J14 = 175;
 
     const [mhr, setMhr] = useState(() => {return 'M/Hr'})
-    var mhrB = penRate;
-    var mhrC = penRate;
-    var mhrD = penRate;
-    var mhrE = penRate;
+    var mhrB = D16;
+    var mhrC = D16;
+    var mhrD = D16;
+    var mhrE = D16;
 
     var tonA = '872321';
     var tonB = '921493';
@@ -94,40 +94,40 @@ const Calculator = () => {
                 <View style={{borderBottomColor: 'black', borderBottomWidth: 3, }}  />
                 <Text style = {styles.sectionTitle}>Drilling Index Calculation</Text>
                 <View style={{borderBottomColor: 'black', borderBottomWidth: 3, }}  />
-                <GenericInput title={'Bit'} val={bit} setFunction={setBit} unit={'in'}></GenericInput>
-                <GenericInput title={'Burden'} val={burden.toString()} setFunction={setBurden} unit={'m'}></GenericInput>
-                <GenericInput title={'Spacing'} val = {spacing.toString()} setFunction={setSpacing} unit={'m'}></GenericInput>
-                <GenericInput title={'Sub-Drilling'} val={subDrilling} setFunction={setSubDrilling} unit={'m'}></GenericInput>
-                <GenericInput title={'Bench'} val={bench.toString()} setFunction={setBench} unit={'m'} ></GenericInput>
-                <GenericInput title={'Rock Density'} val={rockDensityTon.toString()} setFunction={setRockDensityTon} unit={'Ton/m3'}></GenericInput>
-                <GenericInput title={'Rock Density'} val={rockDensityUCS} setFunction={setRockDensityUCS} unit={'UCS'}></GenericInput>
+                <GenericInput title={'Bit'} val={D3} setFunction={setD3} unit={'in'}></GenericInput>
+                <GenericInput title={'Burden'} val={D4.toString()} setFunction={setD4} unit={'m'}></GenericInput>
+                <GenericInput title={'Spacing'} val = {D5.toString()} setFunction={setD5} unit={'m'}></GenericInput>
+                <GenericInput title={'Sub-Drilling'} val={D6} setFunction={setD6} unit={'m'}></GenericInput>
+                <GenericInput title={'Bench'} val={D7.toString()} setFunction={setD7} unit={'m'} ></GenericInput>
+                <GenericInput title={'Rock Density'} val={D8.toString()} setFunction={setD8} unit={'Ton/m3'}></GenericInput>
+                <GenericInput title={'Rock Density'} val={D9} setFunction={setD9} unit={'UCS'}></GenericInput>
                 {/* <GenericInput title={'Drilling Index'} val={drillingIndex} setFunction={setDrillingIndex} unit={'Ton/m'}></GenericInput> */}
                 {/* <GenericInput title={'Ton/Hole'} val={tonHole} setFunction={setTonHole} unit={'Ton'}></GenericInput> */}
                 
-                <GenericOutput title='Drilling Index' val={drillingIndex} unit='Ton/m'></GenericOutput>
-                <GenericOutput title={'Ton/Hole'} val={tonHole} unit={'Ton'}></GenericOutput> 
+                <GenericOutput title='Drilling Index' val={D10} unit='Ton/m'></GenericOutput>
+                <GenericOutput title={'Ton/Hole'} val={D11} unit={'Ton'}></GenericOutput> 
 
-                <GenericInput title={'Target Production T/Month'} val={targetProduction} setFunction={setTargetProduction} unit={'T/Month'}></GenericInput>
-                <GenericInput title={'# of Holes Drilled/Unit/Month'} val={numHoles} setFunction={setNumHoles} unit={'Holes/unit/month'}></GenericInput>
-                <GenericInput title={'M/Month'} val={mMonth} setFunction={setMMonth} unit={'M/month'}></GenericInput>
-                <GenericInput title={'Utilized Hours'} val={utilizedHours} setFunction={setUtilizedHours} unit={'hours'}></GenericInput>
-                <GenericInput title={'Current Pen Rate'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></GenericInput> 
+                <GenericInput title={'Target Production T/Month'} val={D12} setFunction={setD12} unit={'T/Month'}></GenericInput>
+                <GenericInput title={'# of Holes Drilled/Unit/Month'} val={D13} setFunction={setD13} unit={'Holes/unit/month'}></GenericInput>
+                <GenericInput title={'M/Month'} val={D14} setFunction={setD14} unit={'M/month'}></GenericInput>
+                <GenericInput title={'Utilized Hours'} val={D15} setFunction={setD15} unit={'hours'}></GenericInput>
+                <GenericInput title={'Current Pen Rate'} val={D16} setFunction={setD16} unit={'Pen Rate'}></GenericInput> 
 
                 <View style={{borderBottomColor: 'black', borderBottomWidth: 3, }}  />
                 <Text style = {styles.sectionTitle}>Actual Production</Text>
                 <View style={{borderBottomColor: 'black', borderBottomWidth: 3, }}  />
 
                 <TableRow isFirst = {false} title={''} holes={'Holes'} metres={'Metres'} hours={'Hours'} setHours = {setHours} mhr={'M/Hr'} setMhr = {setMhr} totalTon = {'Total Ton'} pctTarget = {'% Target'}></TableRow> 
-                <TableRow isFirst = {true} title={'Target Per Rig'} holes={holesA} metres={metresA} hours={hours} setHours = {setHours} mhr={mhr} setMhr = {setMhr} totalTon = {tonA} pctTarget = {targetA}></TableRow> 
-                <TableRow isFirst = {false} title={'Current Scenario'} holes={holesB} metres={metresB} hours={hoursB} setHours = {setHours} mhr={mhrB} setMhr = {setMhr} totalTon = {tonB} pctTarget = {targetB}></TableRow> 
-                <TableRow isFirst = {false} title={'Scenario 1'} holes={holesC} metres={metresC} hours={hoursC} setHours = {setHours} mhr={mhrC} setMhr = {setMhr} totalTon = {tonC} pctTarget = {targetC}></TableRow> 
-                <TableRow isFirst = {false} title={'Scenario 2'} holes={holesD} metres={metresD} hours={hoursD} setHours = {setHours} mhr={mhrD} setMhr = {setMhr} totalTon = {tonD} pctTarget = {targetD}></TableRow> 
-                <TableRow isFirst = {false} title={'Scenario 3'} holes={holesE} metres={metresE} hours={hoursE} setHours = {setHours} mhr={mhrE} setMhr = {setMhr} totalTon = {tonE} pctTarget = {targetE}></TableRow> 
-               {/*  <TableRow isFirst = {true} title={'Yo'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></TableRow> 
-                <TableRow isFirst = {false} title={'Yo'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></TableRow> 
-                <TableRow isFirst = {false} title={'Yo'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></TableRow> 
-                <TableRow isFirst = {false} title={'Yo'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></TableRow> 
-                <TableRow isFirst = {false} title={'Yo'} val={penRate} setFunction={setPenRate} unit={'Pen Rate'}></TableRow>  */}
+                <TableRow isFirst = {true} title={'Target Per Rig'} holes={H10} metres={I10} hours={hours} setHours = {setHours} mhr={mhr} setMhr = {setMhr} totalTon = {tonA} pctTarget = {targetA}></TableRow> 
+                <TableRow isFirst = {false} title={'Current Scenario'} holes={H11} metres={I11} hours={J11} setHours = {setHours} mhr={mhrB} setMhr = {setMhr} totalTon = {tonB} pctTarget = {targetB}></TableRow> 
+                <TableRow isFirst = {false} title={'Scenario 1'} holes={H12} metres={I12} hours={J12} setHours = {setHours} mhr={mhrC} setMhr = {setMhr} totalTon = {tonC} pctTarget = {targetC}></TableRow> 
+                <TableRow isFirst = {false} title={'Scenario 2'} holes={H13} metres={I13} hours={J13} setHours = {setHours} mhr={mhrD} setMhr = {setMhr} totalTon = {tonD} pctTarget = {targetD}></TableRow> 
+                <TableRow isFirst = {false} title={'Scenario 3'} holes={H14} metres={I14} hours={J14} setHours = {setHours} mhr={mhrE} setMhr = {setMhr} totalTon = {tonE} pctTarget = {targetE}></TableRow> 
+               {/*  <TableRow isFirst = {true} title={'Yo'} val={D16} setFunction={setD16} unit={'Pen Rate'}></TableRow> 
+                <TableRow isFirst = {false} title={'Yo'} val={D16} setFunction={setD16} unit={'Pen Rate'}></TableRow> 
+                <TableRow isFirst = {false} title={'Yo'} val={D16} setFunction={setD16} unit={'Pen Rate'}></TableRow> 
+                <TableRow isFirst = {false} title={'Yo'} val={D16} setFunction={setD16} unit={'Pen Rate'}></TableRow> 
+                <TableRow isFirst = {false} title={'Yo'} val={D16} setFunction={setD16} unit={'Pen Rate'}></TableRow>  */}
 
                 <View style={{borderBottomColor: 'black', borderBottomWidth: 3, }}  />  
                 <Text style = {styles.sectionTitle}>Submit Responses?</Text>
@@ -149,9 +149,26 @@ const Calculator = () => {
     )
 };
 Calculator.propTypes = {
-    bench : PropTypes.number,
-    burden : PropTypes.number,
-    spacing : PropTypes.number,
-    rockDensityTon : PropTypes.number,
+    D7 : PropTypes.number,
+    D4 : PropTypes.number,
+    D5 : PropTypes.number,
+    D8 : PropTypes.number,
+
+    H10 : PropTypes.number,
+    H11 : PropTypes.number,
+    H12 : PropTypes.number,
+    H13 : PropTypes.number,
+    H14 : PropTypes.number,
+
+    I10 : PropTypes.number,
+    I11 : PropTypes.number,
+    I12 : PropTypes.number,
+    I13 : PropTypes.number,
+    I14 : PropTypes.number,
+
+    J11 : PropTypes.number,
+    J12 : PropTypes.number,
+    J13 : PropTypes.number,
+    J14 : PropTypes.number,
   };
 export default Calculator;
--- components/calculatorFunctions.js
+++ components/calculatorFunctions.js
@@ -2,6 +2,6 @@ export const tonHoleCalculation = (bench, burden, spacing, rockDensityTon) => {
     return Math.round(bench * burden * spacing * rockDensityTon);
 };
 
-export const drillingIndexCalculation = (burden, spacing, rockDensityTon) =>  {
+export const drillingIndexCalculation_10d = (burden, spacing, rockDensityTon) =>  {
     return Math.round(burden * spacing * rockDensityTon); 
 };
