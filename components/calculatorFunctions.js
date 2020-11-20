e3d5b79422395a0d1dde5cbd3d59d44289f7590e
Daniel Bertak <danber8@ufl.edu>
2020-11-20 17:06:06 -0500

    Added formulas

--- components/calculatorFunctions.js
+++ components/calculatorFunctions.js
@@ -5,3 +5,10 @@ export const tonHoleCalculation = (bench, burden, spacing, rockDensityTon) => {
 export const drillingIndexCalculation = (burden, spacing, rockDensityTon) =>  {
     return Math.round(burden * spacing * rockDensityTon); 
 };
+
+export const H10_func = (L10, D11) => {
+    if (D11 != 0){
+        return Math.round(L10/D11);
+    }
+    return -1;
+}
