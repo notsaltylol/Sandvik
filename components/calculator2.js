2432d606faed14319d18dd3fb0d8efeae38255ac
Leon <kwan.leon@gmail.com>
2020-12-05 16:33:57 -0500

    dth inst pen

--- components/calculator2.js
+++ components/calculator2.js
@@ -105,8 +105,10 @@ const Calculator2 = () => {
     bit_size = D3
     rock_UCS = D9 //units in MPa
     rig = rigs[0]
-    const instant_pen_mtr_per_hr = () => {
-        const D7 = 1/*(Calculator_N22-'Rig Spec'!I49-('Rig Spec'!K49*'Rig Spec'!M49)>0?"Too Deep":E7)*/
+    hole_depth_ft = 20
+    const rot_instant_pen_mtr_per_hr = () => {
+
+        const D7 = (hole_depth_ft-rig.RotaryHeadTravel.SinglepPass-(rig.RotaryHeadTravel.PipeLength*rig.RotaryHeadTravel.LoaderCap)>0?"Too Deep":E7)
         const J7 = 2000 //temp pipe weight
         const I7 = (string(D7)==="Too Deep"?(J7*rig.RotaryHeadTravel.LoaderCap):J7*(D7<1?1:D7+1))
         const J9 = rig.RigPulldown.MaxPulldown/MaxFeedPressure
@@ -116,6 +118,21 @@ const Calculator2 = () => {
         const E19 = (2.18*F9*bit_size)/(0.2*E18*(Rotary_X5)^0.9*(E18/10000))
         const E31 = E19/3.28083
         return E31/60
+    }
+
+    const dth_instant_pen_mtr_per_hr = () => {
+        
+        M68 = [100.70, 69.11, 44.56, 26.29]
+        const dth_M64 = () => {
+            if(rock_UCS<=100) return 100.70
+            else if(rock_UCS<=200) return 69.11
+            else if(rock_UCS<300) return 44.56
+            else return 26.29
+        }
+        const O27 = dth_M64
+        const O20 = O27*3.28084
+        const R31 =  O20/3.28083
+        return R31
     }
 
     const pressHandler = () =>{
